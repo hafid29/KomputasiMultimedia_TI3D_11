d419e630e47a0a534d367582cfadc497902f6ac9
pamungkas93 <dhutadhuta4@gmail.com>
2021-02-27 17:10:06 +0700

    Modul 3

---
+++ Modul 3/Tugas Modul 3-B/Assets/Script/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Player : MonoBehaviour
+{
+ private PlayerInventoryDisplay playerInventoryDisplay;
+ private int totalMedali = 0;
+ void Start()
+ {
+ playerInventoryDisplay = GetComponent < PlayerInventoryDisplay >
+();
+ }
+ void OnTriggerEnter2D(Collider2D hit)
+ {
+ if (hit.CompareTag("Medali"))
+ {
+ totalMedali++;
+ playerInventoryDisplay.OnChangeMedaliTotal(totalMedali);
+ Destroy(hit.gameObject);
+ }
+ }
+}
