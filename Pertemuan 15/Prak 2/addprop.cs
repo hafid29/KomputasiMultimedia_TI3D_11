a6b98ade01ba9aee2ac97a3b68ebca5bd084ff89
pamungkas93 <dhutadhuta4@gmail.com>
2021-06-14 18:51:51 +0700

    Modul 14 & 15

---
+++ Pertemuan 15/Prak 2/addprop.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class addprop : MonoBehaviour
+{
+    public GameObject prop;
+    public Transform targetBone;
+    public Vector3 positionOffset;
+    public Vector3 rotationOffset;
+    public bool destroyTrigger = true;
+
+    // Start is called before the first frame update
+    void OnTriggerEnter ( Collider collision )
+    {
+        if (targetBone.IsChildOf(collision.transform)){
+            bool checkProp = false;
+            foreach(Transform child in targetBone){
+                checkProp = true;
+            }
+
+            if(!checkProp){
+                GameObject newprop;
+                newprop = Instantiate(prop, targetBone.position, targetBone.rotation) as GameObject;
+                newprop.name = prop.name;
+                newprop.transform.parent = targetBone;
+                newprop.transform.localPosition += positionOffset;
+                newprop.transform.localEulerAngles += rotationOffset;
+                if(destroyTrigger)
+                Destroy(gameObject);
+            }
+        }
+    }
+}
